9e7fb94f35536fc4266d84d1bf91cd60a0ced372
mesps <39406481+mesps@users.noreply.github.com>
2018-06-07 14:59:14 -0300

    Update ServerTeste.c

--- Socket/ServerTeste.c
+++ Socket/ServerTeste.c
@@ -9,6 +9,8 @@
 #define LOGIN_MAX_SIZE 16
 #define MAX_CLIENTS 8
 
+struct character jogador[MAX_CLIENTS];
+
 //whatIs recebe o id do cliente e checa se é par(guarda) ou ímpar(ladrão)
 int whatIs (int x) {
 	if (x%2 == 0) return 0;
@@ -19,17 +21,22 @@ int main()
 {
 	char str_buffer[BUFFER_SIZE], aux_buffer[BUFFER_SIZE];
 	int i, quant_connect = 0;
+	int id = 0;
+	char start = 'N';
 
 	serverInit(MAX_CLIENTS);
 	printf("Server is running!!");
 
 	while (1) {	
 	  	//Deve acontecer enquanto o jogo não comecar
-	    int id = acceptConnection();
+	    id = acceptConnection();
 	    if (id != NO_CONNECTION) {
 	    	//Recebe o nome do cliente
 	    	recvMsgFromClient(jogador[id].nome, id, WAIT_FOR_IT);
 	    	strcpy(str_buffer, jogador[id].nome);
+
+	    	//Manda o id para o cliente
+	    	sendMsgToClient(id, sizeof(int), id);
 
 	    	//Checa se o cliente é polícia ou ladrão
 	    	//Func whatIs() retorna 0 para polícia e 1 para ladrão
@@ -51,20 +58,31 @@ int main()
 	      	//Aumenta a quantidade de jogadores conectados
 	      	quant_connect++;
 	    }
+
+	    //Jogador 1 manda se o jogo comecou ou não
+	    if (start == 'N') {
+	    	recvMsgFromClient(start, 0, DONT_WAIT);
+	    	if (start == 'q' || quant_connect == 7) {
+	    		start = 'q';
+	    		broadcast(start, sizeof(char));
+	    	}
+	    }
 	    
 	    //Deve acontecer quando o jogo comecar
-	    //Manda para todos os jogadores a quantidade de jogadores conectados
-	    broadcast(&quant_connect, sizeof(int), int client_id);
+	    if (start == 'q') {
+		    //Manda para todos os jogadores a quantidade de jogadores conectados
+		    broadcast(quant_connect, sizeof(int));
 
-	    //Recebe as structs de todos os jogadores
-	    for (i = 0; i < quant_connect; i++) {
-	    	recvMsgFromClient(client[i], i, DONT_WAIT);
-	    }
+		    //Recebe as structs de todos os jogadores
+		    for (i = 0; i < quant_connect; i++) {
+		    	recvMsgFromClient(jogador[i], i, DONT_WAIT);
+		    }
 
-	    //Manda a struct atualizada dos jogadores para todos
-	    for (i = 0; i < quant_connect; i++) {    
-	    	broadcast(jogador[i], sizeof(character));
-	    }
+		    //Manda a struct atualizada dos jogadores para todos
+		    for (i = 0; i < quant_connect; i++) {    
+		    	broadcast(jogador[i], sizeof(character));
+		    }
+		}
 
 	    //Acontece sempre
 	    //Saber se o jogador se deconectou
